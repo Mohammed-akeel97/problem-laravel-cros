34f11e5db3be471dce68bdc8f3da4111c7daf72f
Mohammed-akeel97 <Mohammed.marco19971997@gmail.com>
2019-07-19 15:30:24 +0300

    routes/api

---
+++ api.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Http\Request;
+
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+Route::group(['middleware' => ['cros']], function () {
+
+    Route::get('/dataoffline','admin\authUser@index');
+    Route::post('/dataoffline','admin\authUser@index');
+
+});
+/*
+
+
+this is page of api routes
+
+*/
